d3693e4b0c9cabf621c45510c40cded920cec39d
Flander404 <109587664+Flander404@users.noreply.github.com>
2024-08-24 00:19:59 +0500

    flex

--- routes/attributeRouter.js
+++ routes/attributeRouter.js
@@ -10,7 +10,7 @@ router.delete('/category/:id', attributeController.deleteAttributeCategory)
 router.post('/', attributeController.createAttribute)
 router.get('/', attributeController.getAllAttributes)
 router.get('/:id', attributeController.getAllProductByAttributeId)
-router.delete('/:id', attributeController.deleteAttribute)
+router.delete('/:name', attributeController.deleteAttribute)
 
 router.post('/product', attributeController.createAttributeProduct)
 router.get('/product/:productId', attributeController.getAllAttributeProducts)
